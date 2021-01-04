636eb65592c0b3f4a6c765fe01a2eb8520c7f5e2
Borrisson <lucborris@gmail.com>
2021-01-04 20:36:14 +0000

    Changed concatenation to temperate literals.

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,5 @@
 const assertEqual = function(actual, expected) {
-  return actual === expected ? console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected) : console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+  return actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !==  ${expected}`);
 };
 
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
