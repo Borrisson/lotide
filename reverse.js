f4e0f1e8ddbcd3b1830d84adf9345ff8807f6003
Borrisson <lucborris@gmail.com>
2021-01-04 23:33:06 +0000

    fixed style with eslint.

--- reverse.js
+++ reverse.js
@@ -2,7 +2,7 @@ const args = process.argv;
 
 args.slice(2).forEach(function(el) {
   let string = "";
-  for(let i = el.length - 1; i >= 0; i--) {
+  for (let i = el.length - 1; i >= 0; i--) {
     string += el[i];
   }
   console.log(string);
