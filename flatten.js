af476273411a09b03204e8578d935a047cd35836
Borrisson <lucborris@gmail.com>
2021-01-05 23:15:52 +0000

    made a recursive flatten function for arrays.

---
+++ flatten.js
@@ -0,0 +1,15 @@
+const flatten = function(array) {
+  let newArray = array.reduce((flat, current) => flat.concat(current), []);
+  for (let el of newArray) {
+    if (Array.isArray(el)) {
+      newArray = flatten(newArray);
+    }
+  }
+  return newArray;
+};
+
+const array = [1, 2, [[3], 4], 5, [[6]]];
+
+console.log(flatten(array));
+
+console.log(array);
