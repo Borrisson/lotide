53ec2be80e514094ce9d58f2391c51c282cd0551
Borrisson <lucborris@gmail.com>
2021-01-06 20:17:46 +0000

    added last test for space characters

--- letterPositions.js
+++ letterPositions.js
@@ -1,9 +1,14 @@
+const assertEqual = function(actual, expected) {
+  return actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !==  ${expected}`);
+};
+
+
 const eqArrays = function(ar1, ar2) {
-  if (ar1.length !== ar2.length) {
+  if(ar1.length !== ar2.length) {
     return false;
   }
-  for (let index of ar1.keys()) {
-    if (ar1[index] !== ar2[index]) {
+  for (let index of ar1.keys()){
+    if(ar1[index] !== ar2[index]) {
       return false;
     }
   }
@@ -11,14 +16,14 @@ const eqArrays = function(ar1, ar2) {
 };
 
 const assertArraysEqual = function(ar1, ar2) {
-  if (eqArrays(ar1, ar2)) {
-    console.log(`✅✅✅ Assertion Passed: ${ar1} === ${ar2}`);
+  if(eqArrays(ar1, ar2)) {
+    console.log(`✅✅✅ Assertion Passed: ${ar1} === ${ar2}`)
   } else {
-    console.log(`❌❌❌ Assertion Failed: ${ar1} !==  ${ar2}`);
+    console.log(`❌❌❌ Assertion Failed: ${ar1} !==  ${ar2}`)
   }
 };
 
-const letterPositions = function(sentence) {
+const letterPositions = function (sentence) {
  
   const results = {};
 
@@ -49,3 +54,4 @@ assertArraysEqual(word["u"], [7, 20]);
 assertArraysEqual(word["s"], [8, 21]);
 assertArraysEqual(word["e"], [9, 16, 22]);
 assertArraysEqual(word["n"], [12]);
+assertEqual(word[" "], undefined);
