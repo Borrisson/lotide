fed3b2803f2013dc1a967fa7e35961329f6db7f2
Borrisson <lucborris@gmail.com>
2021-01-06 20:31:11 +0000

    function takes object and searchByValue paramater to find the key of that value.

---
+++ findKeyByValue.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  return actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !==  ${expected}`);
+};
+
+const findKeyByValue = function(objectToSearch, searchByValue) {
+  for (let [key, value] of Object.entries(objectToSearch)) {
+    if (value === searchByValue) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  action: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
