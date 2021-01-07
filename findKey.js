87edc4209893d3173ad7a4da64c7711da7f7b6ab
Borrisson <lucborris@gmail.com>
2021-01-07 21:11:29 +0000

    esLint style fix

---
+++ findKey.js
@@ -0,0 +1,24 @@
+const findKey = function(object, callback) {
+  for (let [key, value] of Object.entries(object)) {
+    if (callback(value)) {
+      return key;
+    }
+  }
+};
+let testObject = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+let result = findKey(testObject, x => x.stars === 2); // => "noma"
+let result1 = findKey(testObject, X => X.stars === 0);
+const assertEqual = function(actual, expected) {
+  return actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !==  ${expected}`);
+};
+
+assertEqual(result, "noma");
+assertEqual(result1, undefined);
