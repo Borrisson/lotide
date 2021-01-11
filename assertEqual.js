9b82a843b54d803011c10a3c64e1b02cddb0dc45
Borrisson <lucborris@gmail.com>
2021-01-11 13:21:59 +0000

    refactored lotide head and assertEqual to have unit testing seperate

--- assertEqual.js
+++ assertEqual.js
@@ -2,5 +2,4 @@ const assertEqual = function(actual, expected) {
   return actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !==  ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 2);
+module.exports = assertEqual;
