bdd9bdcc161fb61956cae3f6696efa4bfed2259d
Borrisson <lucborris@gmail.com>
2021-01-12 00:03:42 +0000

    formatted eqArray and added test cases

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,5 @@
+const eqObjects = require('./eqObjects');
+
 const eqArrays = function(ar1, ar2) {
   let condition = true;
   if (ar1.length !== ar2.length) {
@@ -7,6 +9,8 @@ const eqArrays = function(ar1, ar2) {
   for (let index of ar1.keys()) {
     if (Array.isArray(ar1[index])) {
       condition = eqArrays(ar1[index], ar2[index]);
+    } else if(typeof ar1[index] === 'object') {
+      condition = eqObjects(ar1[index], ar2[index]);
     } else if (ar1[index] !== ar2[index]) {
       condition = false;
     }
@@ -15,4 +19,3 @@ const eqArrays = function(ar1, ar2) {
 };
 
 module.exports = eqArrays;
-
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,11 +1,40 @@
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+
+describe('#eqArrays', function() {
+  it('returns true for [1,2,3] === [1,2,3]', function() {
+    assert.isTrue(eqArrays([1,2,3], [1,2,3]), true);
+  });
+  it('returns true for ["1", "2", "3"] === ["1", "2", "3"]', function() {
+    assert.isTrue(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+  it('returns true for [1, 2, 3] === [1, 2, 3]', function() {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it('returns true for [[2, 3], [4]] === [[2, 3], [4]]', function() {
+    assert.isTrue(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+  it('returns true for [] === []', function() {
+    assert.isTrue(eqArrays([], []), true);
+  });
+  it('returns true for [{}] === [{}]', function() {
+    assert.isTrue(eqArrays([{}], [{}]), true);
+  });
+  it('returns true for [1, 2, 3] !== [3, 2, 1]', function() {
+    assert.isFalse(eqArrays([1, 2, 3], [3, 2, 1]), 'false')
+  });
+  it('returns true ["1", "2", "3"] !== ["1", "2", 3]', function() {
+    assert.isFalse(eqArrays(["1", "2", "3"], ["1", "2", 3]), 'false')
+  });
+  it('returns true [2, 3], [4]] !== [[2, 3], [4, 5]] ', function() {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), 'false')
+  });
+  it('returns true [[2, 3], [4]] !== [[2, 3], 4] ', function() {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], 4]), 'false')
+  });
+  
+});
+
+// eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
